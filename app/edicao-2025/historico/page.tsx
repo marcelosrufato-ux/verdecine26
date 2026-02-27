5a35f66d68273f46c4560cd250cc3b320dd277eb
Marcelo Rufato <marcelosrufato@gmail.com>
2026-02-27 09:58:18 -0300

    Atualização Links 25 e 26

--- app/edicao-2025/historico/page.tsx
+++ app/edicao-2025/historico/page.tsx
@@ -12,7 +12,7 @@ import {
   Timeline,
   Button,
 } from "@mantine/core";
-import { HeaderSimple } from "@/app/components/Menu/Menu";
+import { HeaderSimple } from "../components/Menu/Menu";
 
 export default function Page() {
   return (
