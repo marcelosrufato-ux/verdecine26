080330121dabd1d6efcfe12cae679d70675ea970
marcelosrufato-ux <marcelosrufato@gmail.com>
2026-05-23 16:14:10 -0300

    Acerto das logos

--- app/edicao-2026/components/Realizacao.tsx
+++ app/edicao-2026/components/Realizacao.tsx
@@ -5,19 +5,19 @@ export default function Realizacao() {
     <>
       <Stack gap={40} className="">
         <div className="">
-          <h4 className="vc_title text-gray-500 ">Realização</h4>
+          <h5 className="vc_title text-gray-500 ">Realização</h5>
 
-          <Image src="/realizacao26.png" alt="Realização" w={100} />
+          <Image src="/realização2026.png" alt="Realização" w={250} />
         </div>
         <div className="">
-          <h4 className="vc_title text-gray-500 ">Apoio</h4>
+          <h5 className="vc_title text-gray-500 ">Apoio</h5>
 
-          <Image src="/apoio26.png" alt="Apoio" w={300} />
+          <Image src="/Apoio2026.png" alt="Apoio" w={300} />
         </div>
         <div className="">
-          <h4 className="vc_title text-gray-500 ">Parceiros</h4>
+          <h5 className="vc_title text-gray-500 ">Parceiros</h5>
 
-          <Image src="/parceiros26.png" alt="Parceiros" w={400} />
+          <Image src="/parceiros2026.png" alt="Parceiros" w={300} />
         </div>
       </Stack>
     </>
